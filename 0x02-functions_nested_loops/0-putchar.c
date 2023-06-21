34e0b663050c6c960976f66e3df392d9b16cd2b2
NgaruiyaFaith <wangarifaith2020@gmail.com>
2023-06-21 01:24:41 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+    char word[] = "_putchar";
+    int i = 0;
+
+    while (word[i] != '\0') 
+{
+        putchar(word[i]);
+        i++;
+    }
+
+    putchar('\n');
+
+    return 0;
+}
