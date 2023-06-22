43efab8ead0ac37072a663ca9fee7d9bbc0176be
NgaruiyaFaith <wangarifaith2020@gmail.com>
2023-06-22 02:16:02 -0700

    Checking for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - Checks for alphabetic character
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for lowercase and uppercase. 0 for other
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122) || (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
