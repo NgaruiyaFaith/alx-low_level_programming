4cad55a12fc88f5c4c64cbeea4acd706926a97c2
NgaruiyaFaith <wangarifaith2020@gmail.com>
2023-06-22 03:03:49 -0700

    Fixed the code to align with betty coding style

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * print_sign - Determines if the input number is greater, equal or less than zero.
+ * print_sign - Determines if the sign of the input number.
+ *
  *
  * @n: The input number as an integer.
  *
