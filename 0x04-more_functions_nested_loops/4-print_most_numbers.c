d63fe80f90a587c57fddfe9a650c2003bf001885
NgaruiyaFaith <wangarifaith2020@gmail.com>
2023-06-24 06:40:56 -0700

    Betty style fixing

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * print_most_numbers - checks for checks for a digit (0 through 9).
+ * print_most_numbers - Checks for cheks for a digit (0 through 9)
  *
- * Return: Always 0.
+ * Return: Always 0
  */
-
 void print_most_numbers(void)
 {
 	int c;
@@ -14,10 +13,10 @@ void print_most_numbers(void)
 	{
 		if (c != 50)
 		{
-			 if (c != 52)
-			 {
-				 _putchar(c);
-			 }
+			if (c != 52)
+			{
+				_putchar(c);
+			}
 		}
 	}
 	_putchar('\n');
