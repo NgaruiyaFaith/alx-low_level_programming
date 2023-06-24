ec5edfb901a53dc25e28522ee2ce2cccfdd7cb12
NgaruiyaFaith <wangarifaith2020@gmail.com>
2023-06-24 06:52:47 -0700

    Fixed the code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,7 +5,7 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i,j;
 
 	for (i =0; i < 10; 1++)
 	{
@@ -13,7 +13,8 @@ void more_numbers(void)
 		{
 			if (j >= 10)
 				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+			
+			        _putchar(j % 10 + '0');
 		}
 		_putchar('\n')
 	}
