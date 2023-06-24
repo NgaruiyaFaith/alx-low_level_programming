74687fa6aa710da5324d7923f2f71a2ed9559b0e
NgaruiyaFaith <wangarifaith2020@gmail.com>
2023-06-24 06:31:37 -0700

    Fixed betty coding style

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,15 +5,16 @@
  *
  * Return: Always 0.
  */
+
 void print_most_numbers(void)
 {
 	int c;
 
 	for (c = 48; c < 58; c++)
 	{
-		if (c ! = 50)
+		if (c != 50)
 		{
-			 if (c ! = 52)
+			 if (c != 52)
 			 {
 				 _putchar(c);
 			 }
