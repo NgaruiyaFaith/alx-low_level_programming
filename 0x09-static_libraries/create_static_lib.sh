6279a37bec839220973523817b0a038d68925f5e
NgaruiyaFaith <wangarifaith2020@gmail.com>
2023-10-19 01:21:27 -0700

    Directories removal

--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Compile all .c files into .o files
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
-
-#Create a static library named liballa.a from all .o files
-ar -rc liball.a *.o
